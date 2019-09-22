bbe4d4b3dfa62c6f3b24301d0a408cc9fd202035
y0ssi10 <iwai0621@gmail.com>
2019-09-22 16:57:21 +0900

    Add solution for Decode String

---
+++ decode_string/solution.py
@@ -0,0 +1,31 @@
+# Problem:
+#   Given an encoded string, return its decoded string.
+#   The encoding rule is: k[encoded_string],
+#   where the encoded_string inside the square brackets is being repeated exactly k times.
+#   Note that k is guaranteed to be a positive integer.
+#   You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+#   Furthermore, you may assume that the original data does not contain any digits and
+#   that digits are only for those repeat numbers, k.
+#   For example, there won't be input like 3a or 2[4].
+#
+# Examples:
+#   s = "3[a]2[bc]", return "aaabcbc".
+#   s = "3[a2[c]]", return "accaccacc".
+#   s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        while s.find(']') != -1:
+            end_index = s.find(']')
+            start_index = s[:end_index].rfind('[')
+            num_str = ''
+            for c in s[:start_index][::-1]:
+                if c.isdigit():
+                    num_str = c + num_str
+                else:
+                    break
+            encoded_string = self.decodeString(s[:end_index][start_index + 1:])
+            s = s[:start_index - len(num_str)] + (int(num_str) * encoded_string) + s[end_index + 1:]
+
+        return s
