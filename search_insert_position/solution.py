97bfed44c9f710c3f5c90013d21763fd12484a21
y0ssi10 <iwai0621@gmail.com>
2020-03-04 17:49:03 +0900

    Add another solution for Search Insert Position

--- search_insert_position/solution.py
+++ search_insert_position/solution.py
@@ -17,3 +17,26 @@ class Solution:
                 return i
 
         return size
+
+    # divede and conquer
+    def search_insert_2(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+
+        left_index = 0
+        right_index = len(nums) - 1
+
+        while left_index + 1 < right_index:
+            mid = (left_index + right_index) // 2
+            if nums[mid] < target:
+                left_index = mid
+            if nums[mid] > target:
+                right_index = mid
+
+        if nums[right_index] < target:
+            return len(nums)
+
+        if nums[left_index] > target:
+            return 0
+
+        return left_index + 1
