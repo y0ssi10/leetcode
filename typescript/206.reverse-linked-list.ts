cb9e5c21f58e71ad00cb1fdaadc367ccf3878d90
y0ssi10 <iwai0621@gmail.com>
2022-01-27 02:17:55 +0900

    Solve the 'Reverse Linked List' problem with TypeScript

---
+++ typescript/206.reverse-linked-list.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=206 lang=typescript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let dummy = null;
+  let node = head;
+  while (node !== null) {
+    const curr = node;
+    node = node.next;
+    curr.next = dummy;
+    dummy = curr;
+  }
+
+  return dummy;
+};
+// @lc code=end
+
