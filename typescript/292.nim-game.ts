a359c8cc75b6b7170569173300aecbde5f6a19e7
y0ssi10 <iwai0621@gmail.com>
2025-04-25 00:26:04 +0900

    feat(nim-game.ts): add solution for LeetCode problem 292 to determine if a player can win the Nim game

---
+++ typescript/292.nim-game.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=292 lang=typescript
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+function canWinNim(n: number): boolean {
+  return n % 4 !== 0;
+};
+// @lc code=end
